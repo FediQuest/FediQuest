ab50f534bdf65cd96bdd32570dc64dc3bb720477
FediQuest <fediquest@tutamail.com>
2026-04-20 13:03:53 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+/**
+ * FediQuest Root Build Configuration
+ * Top-level build file for configuring common settings across all sub-projects.
+ * Uses Gradle toolchain resolver for automatic Java 17 provisioning.
+ */
 plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
